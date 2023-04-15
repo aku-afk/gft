f11e0a8ce161d68944e915c9f0e445ee3755bad8
mamat <64795908+aku-afk@users.noreply.github.com>
2023-04-16 00:27:32 +0700

    Create webview.php

---
+++ lib/webview.php
@@ -0,0 +1,77 @@
+<?php
+
+include 'pnp-gft.php';
+
+if (isset($_POST['start'])) {
+    $lk = $_POST['src'];
+    $pr = $_POST['par'];
+
+    $view = gftu($lk, $pr);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <table border="1">
+            <tr>
+                <td>Link</td>
+                <td>
+                    <input type="text" name="src" value="<?php echo $lk; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Parameter</td>
+                <td>
+                    <input type="text" name="par" value="<?php echo $pr; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" value="start" name="start">
+                </td>
+            </tr>
+        </table>
+    </form>
+    <table border="1">
+        <tr>
+            <td>Title</td>
+            <td>
+                <pre>
+                    <?php echo $view['title']; ?>
+                </pre>
+            </td>
+        </tr>
+        <tr>
+            <td>Parameter</td>
+            <td>
+                <pre>
+                    <?php echo $view['params']; ?>
+                </pre>
+            </td>
+        </tr>
+        <tr>
+            <td>Content</td>
+            <td>
+                <pre>
+                    <?php echo $view['content']; ?>
+                </pre>
+            </td>
+        </tr>
+    </table>
+
+    <br><br><br><br>
+
+    <pre>
+        <?php print_r($view['raw']); ?>
+    </pre>
+</body>
+</html>
